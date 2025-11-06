022fe5440378b1e9010c905adf4b579a903c3f92
xiyuesaves <xiyuesaves@gmail.com>
2025-11-06 15:47:58 +0800

    feat: 添加qwqnt核心类型定义库，优化类型定义

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,13 +1,28 @@
-/// <reference path="/types/proxyIpcMessage.d.ts" />
-
-declare const Logs: any;
-declare const qwqnt: any;
-
-interface Window {
-  navigation: any;
-  qwqnt: any;
-}
-
 declare global {
-  var IpcInterceptor: IpcInterceptorType;
+  // LiteLoader 兼容
+  declare const LiteLoader: any;
+
+  // ipc-logger 兼容
+  declare const Logs: any;
+
+  interface Window {
+    navigation: any;
+    qwqnt: any;
+    lt_logs: () => void;
+    lt_errors: () => void;
+  }
+
+  interface Peer {
+    chatType: 1 | 2 | 100;
+    guildId: string;
+    peerUid: string;
+  }
+
+  interface Element {
+    lt_slot?: HTMLElement;
+    __VUE__?: any[];
+    __vue_app__?: any;
+  }
 }
+
+export {};
